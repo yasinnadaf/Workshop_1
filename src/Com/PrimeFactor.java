7c530a1af654bcbf8b8a2ab07ac88090654d8294
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-11 12:10:59 +0530

    prime factor

--- src/Com/PrimeFactor.java
+++ src/Com/PrimeFactor.java
@@ -7,9 +7,13 @@ public class PrimeFactor {
         System.out.println("Enter a number to find its factor");
         Scanner s=new Scanner(System.in);
         int factorNumber=s.nextInt();
-        for(int i=2; i<=factorNumber; i++){
-            if(factorNumber % i == 0);
-            System.out.print(i +"\t");
+        for(int i=2; i<factorNumber; i++){
+            while(factorNumber % i == 0){
+                System.out.print(i +" ");
+                factorNumber=factorNumber/i;
+            }
+            if(factorNumber>2)
+            System.out.print(factorNumber);
         }
 
     }
